286eca40c224da1043cfb2a3d8936a4dab2a3a75
PretoX <pretoxmatrix@posteo.de>
2017-12-22 22:04:45 +0100

    Added Javadoc.

--- src/main/java/main/BotUtils.java
+++ src/main/java/main/BotUtils.java
@@ -4,17 +4,32 @@ import sx.blah.discord.api.*;
 import sx.blah.discord.util.*;
 import sx.blah.discord.handle.obj.*;
 
-
+/**
+ * Helper class with useful methods
+ * @author Preto95
+ * @version 1
+ */
 
 public class BotUtils {
 
-    static String bot_prefix = "!";
+    static String bot_prefix = "!"; //To filter the commands
 
+    /**
+     * creates the client with ClientBuilder
+     * @param token Token for the bot
+     * @param login login or not
+     * @return The discord client
+     */
     static IDiscordClient createClient(String token, boolean login)
     {
         return new ClientBuilder().withToken(token).build();
     }
 
+    /**
+     * Sends message to the defined channel
+     * @param channel Channel for the message
+     * @param message Message to send
+     */
     public static void sendMessage(IChannel channel, String message)
     {
         RequestBuffer.request(() -> {
