d219962363c3d507fd482e72d6b02bc88cd94759
PretoX <pretoxmatrix@posteo.de>
2017-12-22 22:05:20 +0100

    Added Javadoc.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,12 @@ import java.lang.ClassLoader;
 import java.io.*;
 import java.util.Properties;
 
+/**
+ * Main class
+ * @author Preto95
+ * @version 2
+ */
+
 public class Main {
 
     public static void main (String[] args)
